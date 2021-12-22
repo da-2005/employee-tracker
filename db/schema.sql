a5ca2f0e6a316e0794f763cfe6f6c2fdcabd4bba
kdot <alvarezderrick0@gmail.com>
2021-12-21 22:24:59 -0800

    fixed typos

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,12 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30)
 );
 
-CREATE TABLE role(
-    id INT PRIMARY KEY,
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
@@ -18,11 +18,11 @@ CREATE TABLE role(
     ON DELETE SET NULL
 );
 
-CREATE TABLE employee(
-    id INT PRIMARY KEY,
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id: INT,
+    role_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON DELETE SET NULL,
@@ -30,4 +30,4 @@ CREATE TABLE employee(
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
     ON DELETE SET NULL
-)
+);
