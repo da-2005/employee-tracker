9227199cfaf5cf8a1fa456ad6f7c833708c0547f
kdot <alvarezderrick0@gmail.com>
2021-12-25 15:49:35 -0800

    added SQL queries that executes once the showDepartment, showRoles, and showEmployees functions are hit

---
+++ db/queries.sql
@@ -0,0 +1,19 @@
+-- SQL queries that will go into the .query method in employee_db.js
+
+-- Shows all departments
+SELECT id AS ID, department_name AS department FROM department;
+
+-- Shows all roles
+SELECT role.id AS ID, title, department_name AS department, salary  
+FROM department
+JOIN role
+ON department.id = role.department_id;
+
+--Shows all employees
+SELECT employee.id, employee.first_name, employee.last_name, title, department_name AS department, salary, CONCAT (manager.first_name, " ", manager.last_name) AS manager
+FROM employee
+JOIN role 
+ON employee.role_id = role.id
+JOIN department
+ON role.department_id = department.id
+LEFT JOIN employee manager ON employee.manager_id = manager.id;
